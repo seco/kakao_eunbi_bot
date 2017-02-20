5dbb00d01872ca51a6afd29c8c0e9cbbd7d23c90
최은비 <ebchoi@bulkytree.com>
2017-02-20 17:19:40 +0900

    실수 정정

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/bot');
+var bot = require('./routes/bot');
 
 var app = express();
 
