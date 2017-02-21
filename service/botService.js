3d532c9124f50052f591ecdea518e17142cf1e5d
최은비 <ebchoi@bulkytree.com>
2017-02-21 18:52:20 +0900

    테스트용

--- service/botService.js
+++ service/botService.js
@@ -7,7 +7,7 @@ bot.selectMenu = (content) => {
     console.log('content', content);
     switch (content) {
         case message.buttons[0]: //밥집
-            return(message.baseType(`여기에 밥집 전화번호를 입력하면 됨`));
+            return(message.baseType(`미스터피자(평촌점) : 031-385-0111`));
             break;
         case message.buttons[1]: //술집
             return(message.baseType(`여기에 술집 전화번호를 입력하면 됨`));
