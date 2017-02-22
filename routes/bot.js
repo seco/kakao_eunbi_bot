82b98b40fb6320dd2c7285562b1ad2a2a943ac9a
최은비 <ebchoi@bulkytree.com>
2017-02-22 16:38:22 +0900

    test

--- routes/bot.js
+++ routes/bot.js
@@ -19,6 +19,8 @@ router.post('/message', (req, res) => {
 	let type = req.body.type;
 	let content = req.body.content;
 
+	console.log('req', req, req.cache);
+
 	bot.selectMenu(req, content, (err, results) => {
 		if (err) throw err;
 
